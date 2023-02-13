1aaa51d535a8dfb893c059069fb856c97d0f940d
limseungchae <tmdco12@naver.com>
2023-02-13 11:41:09 +0900

    data.go.kr - 미세먼지 출력 (xml), 등급

--- crwaling04.js
+++ crwaling04.js
@@ -7,52 +7,54 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');    // 파일시스템 관리 라이브러리
 const path = require('path');  // 파일경로 관리 라이브러리
+const { XMLParser } = require('fast-xml-parser'); // xml 처리기 라이브러리
 
 async function main() {     // 비동기 I/O 지원 함수 정의
 
     // 접속한 url, 쿼리스트링, user-agent 헤더 지정
+    // 인증 vs 인가
     const URL = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty';
     const params = {'serviceKey': 'GPxX5sf+Qr9HYeoalPoSjzj+J9DtyQNNP++sIOjriI7SJBy+f7Ti4Q4mbzsLInqHkhBygKyArX8svRO8LatPGg==',
-        'returnType': 'json','sidoName': '서울', 'numOfRows':1000, 'ver':1.3
+        'returnType': 'xml','sidoName': '서울', 'numOfRows':1000, 'ver':1.3
     };
 
     const headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78'};
 
     // axios로 접속해서 대기오염 정보를 받아옴
-    const json = await axios.get(URL,{
+    const xml = await axios.get(URL,{
         params : params, headers : headers
     }); // 서버 요청시 User-Agent 헤더 사용
 
     // 밥아온 데이터 잠시 확인
-    // console.log(json.data);
+    // console.log(xml.data);
+
+    // XML을 JSON으로 변환하기
+    const parser = new XMLParser();
+    let json = parser.parse(xml.data);
+
 
     // JSON 으로 불러오기
-    let items = json.data['response']['body']['items'];
-    // console.log(items);
+    let items = json['response']['body']['items'];
+    // console.log(items['item']);
 
-    // 미세먼지 정보 출력
-    // pm25Value는 출력 안됨!!
-    for(let item of items) {
+    // // 미세먼지 정보 출력
+    for(let item of items['item']) {
         console.log(item.sidoName, item.stationName,
             item.pm10Value, item.pm25Value,
             item.pm10Grade, item.pm25Grade,
             pmGrade(item.pm10Grade), pmGrade(item.pm25Grade),
             item.dataTime);
-    }
-
     };
 
-    // 등급별 이모지
-    // 😍 😐 😰 😱
-    //  1  2  3  4
-    let pmGrade = (val) => {
-        // let emoji = '😱'
-        // if (val == '1') emoji = '😍';
-        // else if (val == '2') emoji = '😐';
-        // else if (val == '3') emoji = '😰';
-        let emojis = ['😍','😐','😰','😱'];
+};
 
-        return emojis[parseInt(val) - 1];
-    };
+// 등급별 이모지
+// 😍 😐 😰 😱
+//  1  2  3  4
+let pmGrade = (val) => {
+    let emojis = ['😍','😐','😰','😱'];
+
+    return emojis[parseInt(val) - 1];
+};
 
 main();
