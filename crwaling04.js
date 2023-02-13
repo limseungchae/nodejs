3678974d29ebe5e1c8ca861fac3d06cbd3e28e8c
limseungchae <tmdco12@naver.com>
2023-02-13 10:23:52 +0900

    data.go.kr - 미세먼지 출력 (json), 등급

--- crwaling04.js
+++ crwaling04.js
@@ -13,7 +13,7 @@ async function main() {     // 비동기 I/O 지원 함수 정의
     // 접속한 url, 쿼리스트링, user-agent 헤더 지정
     const URL = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty';
     const params = {'serviceKey': 'GPxX5sf+Qr9HYeoalPoSjzj+J9DtyQNNP++sIOjriI7SJBy+f7Ti4Q4mbzsLInqHkhBygKyArX8svRO8LatPGg==',
-        'returnType': 'json','sidoName': '전국', 'numOfRows':1000
+        'returnType': 'json','sidoName': '서울', 'numOfRows':1000, 'ver':1.3
     };
 
     const headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78'};
@@ -34,9 +34,25 @@ async function main() {     // 비동기 I/O 지원 함수 정의
     // pm25Value는 출력 안됨!!
     for(let item of items) {
         console.log(item.sidoName, item.stationName,
-            item.pm10Value, item.pm25Value,item.dataTime);
+            item.pm10Value, item.pm25Value,
+            item.pm10Grade, item.pm25Grade,
+            pmGrade(item.pm10Grade), pmGrade(item.pm25Grade),
+            item.dataTime);
     }
 
-};
+    };
+
+    // 등급별 이모지
+    // 😍 😐 😰 😱
+    //  1  2  3  4
+    let pmGrade = (val) => {
+        // let emoji = '😱'
+        // if (val == '1') emoji = '😍';
+        // else if (val == '2') emoji = '😐';
+        // else if (val == '3') emoji = '😰';
+        let emojis = ['😍','😐','😰','😱'];
+
+        return emojis[parseInt(val) - 1];
+    };
 
 main();
