b71baa47dcac0cf9f92466a0180f651b9034b157
limseungchae <tmdco12@naver.com>
2023-02-14 16:59:59 +0900

    mariadb zipcode2013

---
+++ dbconfig2.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'fullstacks.cmsyuhhb6fnz.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin', password: 'fullstack_2023',
+    database: 'fullstacks'
+}
+
---
+++ mariadb01.js
@@ -0,0 +1,32 @@
+const mariadb = require('mariadb');
+const dbconfig = require('./dbconfig2');
+const oracledb = require("oracledb");
+
+async function main() {
+    const sql = ' select distinct sido from zipcode2013 ';
+
+    let conn = null;
+
+    try {
+        conn = await mariadb.createConnection(dbconfig);
+
+        let result = await conn.execute(sql);
+        // console.log(result);
+
+        for(let row of result) {
+            console.log(row.sido);
+        }
+
+    } catch (ex) {
+        console.error(ex)
+    } finally {
+        if (conn) {
+            try { await conn.close(); }
+            catch (ex) {
+                console.error(ex);
+            }
+        }
+    }
+}; // main
+
+main();
